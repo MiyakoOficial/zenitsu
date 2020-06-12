534f9603f1729d21acd8ade68099a9a4879ed260
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-06-12 17:53:23 -0400

    glitch update

--- bot.js
+++ bot.js
@@ -1,3 +1,25 @@
+//ESTE CODIGO NO AFECTARA SU BOT, SCRIPT DE ARRANQUE
+
+const http = require('http');
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get("/", function (request, response) {
+    response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/", (request, response) => {
+    response.sendStatus(200);
+});
+
+app.listen(process.env.PORT);
+
+setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('config.json');
