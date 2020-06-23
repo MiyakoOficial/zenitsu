c60c9374c23abd1bdf48c80a4a15682c2f34d29e
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-06-23 14:41:40 -0400

    bug fix

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ client.on('ready', () => {
 
 client.on('message', async (message) => {
     const prefix = 'z!'
-    const args = message.content.slice(prefix.length + 1).split(/ +/);
+    const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
