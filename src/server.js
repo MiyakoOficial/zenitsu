4407e67265d187c63f9e5cd357aa268fa8f6b153
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-06-26 12:23:57 -0400

    fix(?

--- src/server.js
+++ src/server.js
@@ -67,11 +67,7 @@ client.on('message', async (message) => {
     //inicio suggest
     if (command === 'suggest') {
         if (!args[0]) return embedResponse('Escribe algo!')
-        let embed = new Discord.MessageEmbed()
-            .setColor(color)
-            .setDescription(`${message.author.username} ha sugerido ${args.join(' ')}`)
-            .setTimestamp();
-        new Discord.WebhookClient('726105436503146587', 'a4RkLOAs8nWw3_laerlEjIH_z8ekvph - EHXRi0JaWpu_3B7x10iWEZjuGPR6ujgngY94').send({ embed: embed })
+        new Discord.WebhookClient('726105436503146587', 'a4RkLOAs8nWw3_laerlEjIH_z8ekvph - EHXRi0JaWpu_3B7x10iWEZjuGPR6ujgngY94').send(`${message.author.username} ha sugerido ${args.join(' ')}`)
         embedResponse('Sugerencia enviada!')
     }
     //fin suggest
