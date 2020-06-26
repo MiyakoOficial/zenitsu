bcb3f3e7cf83933003462e3723563d5ddd882c91
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-06-26 12:27:31 -0400

    fix?

--- src/server.js
+++ src/server.js
@@ -67,7 +67,11 @@ client.on('message', async (message) => {
     //inicio suggest
     if (command === 'suggest') {
         if (!args[0]) return embedResponse('Escribe algo!')
-        new Discord.WebhookClient('726105436503146587', 'a4RkLOAs8nWw3_laerlEjIH_z8ekvph-EHXRi0JaWpu_3B7x10iWEZjuGPR6ujgngY94').send(`${message.author.username} ha sugerido ${args.join(' ')}`)
+        let embed = new Discord.MessageEmbed()
+            .setTimestamp()
+            .setColor(color)
+            .setDescription(`${message.author.username} ha sugerido ${args.join(' ')}`)
+        new Discord.WebhookClient('726105436503146587', 'a4RkLOAs8nWw3_laerlEjIH_z8ekvph-EHXRi0JaWpu_3B7x10iWEZjuGPR6ujgngY94').send({ embed: embed })
         embedResponse('Sugerencia enviada!')
     }
     //fin suggest
