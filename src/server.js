8249e9ca4c8ecb897aa150b3ac0bef58c7b03573
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-06-29 22:01:17 -0400

    testing

--- src/server.js
+++ src/server.js
@@ -280,6 +280,9 @@ client.on('roleUpdate', async (oldRole, newRole) => {
             .setTimestamp()
             .setFooter(newRole.guild.name, newRole.guild.iconURL({ format: 'png', size: 2048 }))
             .setColor(color)
+        newRole.guild.fetchAuditLogs()
+            .then(audit => console.log(audit.entries.first()))
+            .catch(console.error);
         if (data.channellogs === 'defaultValue') return console.log('No se ha establecido ningun canal en el servidor ' + newRole.guild.name)
         if (err) return console.log(err);
         if (!data) return console.log('Error!')
