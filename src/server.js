1e150fa0d7dc354d25865d3864acdd48766b326e
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-07-01 19:18:53 -0400

    xd

--- src/server.js
+++ src/server.js
@@ -7,7 +7,8 @@ const client = new Discord.Client();
 const mongoose = require('mongoose');
 const { info, error } = require('console');
 const mil = require("ms")
-
+const tresenraya = require('tresenraya');
+const juego = new tresenraya(opciones);
 
 /*function duration(s) {
     var ms = s % 1000;
@@ -114,6 +115,44 @@ client.on('message', async (message) => {
         } catch (e) { return errorEmbed(e) }
     }
     //fin de dm
+
+    if (command === '3enraya') {
+        const usuario = message.mentions.users.first();
+        if (!usuario) return message.channel.send('Menciona a alguien');
+
+        const partida = new tresenraya({ jugadores: [message.author.id, usuario.id] });
+
+        partida.on('ganador', (jugador, tablero, paso) => { // cuando encuentra a algún ganador se emite el evento 'ganador'
+
+            message.channel.send('¡Ha ganado ' + client.users.get(jugador).username + ' en esta partida! Después de `' + paso + ' pasos.`\n\n' + tablero.string + '\n\nLo siento, ' + client.users.get(partida.perdedor).username + '... 😦');
+
+        });
+
+        partida.on('empate', (jugadores, tablero, paso) => { // si se produce un empate se emite el evento 'empate'
+
+            message.channel.send('¡Ha habido un empate entre ' + jugadores.map(x => client.users.get(x).username).join(' y ') + '!');
+
+        });
+
+        message.channel.send('Empieza ' + client.users.get(partida.turno.jugador).username + ', elige un número del 1 al 9 [`' + partida.turno.ficha + '`]\n\n' + partida.tablero.string);
+
+        const colector = message.channel.createMessageCollector(msg => msg.author.id === partida.turno.jugador && !isNaN(msg.content) && (Number(msg.content) >= 1 && Number(msg.content) <= 9) && partida.disponible(msg.content) && !partida.finalizado);
+
+        colector.on('collect', (msg) => {
+
+            partida.elegir(msg.content); // elegir la posición dependiendo del contenido del mensaje recolectado
+
+            if (partida.finalizado) {
+
+                colector.stop();
+                return;
+
+            } // si la partida ya ha finalizado (ya sea por que alguien ha ganado o ha habido un empate), para el colector y retorna nada
+
+            message.channel.send('Turno de ' + client.users.get(partida.turno.jugador).username + ' [`' + partida.turno.ficha + '`]\n\n' + partida.tablero.string);
+
+        });
+    };
 
     //!inicio de blockchannels
     if (command === 'blockchannels') {
