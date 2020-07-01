72a826460aeed2d03591d6da47e588b7e6f06a86
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-07-01 13:05:27 -0400

    fixed!

--- src/server.js
+++ src/server.js
@@ -284,8 +284,8 @@ client.on('messageDelete', async (message) => {
 });
 
 client.on('roleUpdate', async (oldRole, newRole) => {
-    const listaAddeds = ['']
-    const listaRemoveds = ['']
+    const listaAddeds = ['.']
+    const listaRemoveds = ['.']
     if (!oldRole.permissions.has('ADMINISTRATOR') && newRole.permissions.has('ADMINISTRATOR')) listaAddeds.push('Administrator');
     if (oldRole.permissions.has('ADMINISTRATOR') && !newRole.permissions.has('ADMINISTRATOR')) listaRemoveds.push('Administrator');
 
