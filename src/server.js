d1fbb1fcbbda4e6d84bdc60eb91a7fd178abad0a
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-07-03 18:57:22 -0400

    fixing

--- src/server.js
+++ src/server.js
@@ -93,52 +93,11 @@ client.on('message', async (message) => {
                 .addField('Comandos', 'z!help, z!setlogs, z!canal, z!suggest, z!bugreport')
                 .addField('Extras', 'z!txt, z!ping')
                 .addField('Administración', 'z!blockchannels')
-                .addField('Diversion', 'pronto...')
+                .addField('Diversión', 'pronto...')
                 .setThumbnail(client.user.displayAvatarURL({ format: 'png', size: 2048 }))
         }).catch(err => console.log(err))
     }
     //fin de help
-    //inicio de 3enraya
-    /*
-        if (command === '3enraya') {
-            const usuario = message.mentions.users.first();
-            if (!usuario) return message.channel.send('Menciona a alguien');
-    
-            const partida = new tresenraya({ jugadores: [message.author.id, usuario.id] });
-    
-            partida.on('ganador', (jugador, tablero, paso) => { // cuando encuentra a algún ganador se emite el evento 'ganador'
-    
-                message.channel.send('¡Ha ganado ' + client.users.cache.get(jugador).username + ' en esta partida! Después de `' + paso + ' pasos.`\n\n' + tablero.string + '\n\nLo siento, ' + client.users.cache.get(partida.perdedor).username + '... 😦');
-    
-            });
-    
-            partida.on('empate', (jugadores, tablero, paso) => { // si se produce un empate se emite el evento 'empate'
-    
-                message.channel.send('¡Ha habido un empate entre ' + jugadores.map(x => client.users.cache.get(x).username).join(' y ') + '!');
-    
-            });
-    
-            message.channel.send('Empieza ' + client.users.cache.get(partida.turno.jugador).username + ', elige un número del 1 al 9 [`' + partida.turno.ficha + '`]\n\n' + partida.tablero.string);
-    
-            const colector = message.channel.createMessageCollector(msg => msg.author.id === partida.turno.jugador && !isNaN(msg.content) && (Number(msg.content) >= 1 && Number(msg.content) <= 9) && partida.disponible(msg.content) && !partida.finalizado);
-    
-            colector.on('collect', (msg) => {
-    
-                partida.elegir(msg.content); // elegir la posición dependiendo del contenido del mensaje recolectado
-    
-                if (partida.finalizado) {
-    
-                    colector.stop();
-                    return;
-    
-                } // si la partida ya ha finalizado (ya sea por que alguien ha ganado o ha habido un empate), para el colector y retorna nada
-    
-                message.channel.send('Turno de ' + client.users.cache.get(partida.turno.jugador).username + ' [`' + partida.turno.ficha + '`]\n\n' + partida.tablero.string);
-    
-            });
-        };
-    */
-    //fin de 3enraya
     //!inicio de blockchannels
     if (command === 'blockchannels') {
         if (!message.guild.me.hasPermission('MANAGE_CHANNELS')) return errorEmbed('No tengo el permiso MANAGE_CHANNELS');
