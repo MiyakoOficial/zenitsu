565031347d214095c0c737d4ca6658f7fdb43913
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-07-16 20:57:32 -0400

    uber copia :flushed:

--- src/server.js
+++ src/server.js
@@ -1000,8 +1000,8 @@ client.on('error', async (error) => {
 
 client.on('message', async (msg) => {
     msg.channel.messages.fetch({ limit: 3 }).then(m => {
-        let a = m.array()
-        if (a[0].content.toLowerCase() === a[1].content.toLowerCase() === a[2].content.toLowerCase) {
+        let a = m.filter(E => !E.author.bot).array()
+        if (a[0].content.toLowerCase() === a[1].content.toLowerCase() && a[1].content.toLowerCase() === a[2].content.toLowerCase()) {
             msg.channel.send(a[2].content)
         }
     })
