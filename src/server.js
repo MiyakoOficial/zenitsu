8519bb7631af608e1b97168c955e26cd782b3c66
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-07-29 12:19:05 -0400

    !

--- src/server.js
+++ src/server.js
@@ -396,7 +396,7 @@ client.on('message', async (message) => {
         await LogsModel.findOne({ id: message.guild.id }, async (err, data) => {
             if (err) return console.log(err);
 
-            if (!data) return embedResponse("Este servidor no tiene definido un canal de logs").catch(error => { enviarError(error, message.author) })
+            if (!data) return embedResponse("Este servidor no tiene definido un canal de logs!").catch(error => { enviarError(error, message.author) })
             if (!message.guild.channels.cache.filter(a => a.type === 'text').map(a => a.id).includes(data.channellogs)) return embedResponse('El canal en la base de datos no existe!').catch(error => { enviarError(error, message.author) })
             else return embedResponse(`Logs: <#${data.channellogs}>(${data.channellogs})`).catch(error => { enviarError(error, message.author) })
         });
