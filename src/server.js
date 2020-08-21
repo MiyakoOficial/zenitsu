bb6dcbadeb6f1f051f5d4047b442f4c6a021194f
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-08-21 19:00:31 -0400

    blacklist command

--- src/server.js
+++ src/server.js
@@ -177,8 +177,12 @@ client.on('message', async (message) => {
 
     if (!message.content.startsWith(prefix)) return;
     if (message.content.length < prefix.length + 1) return;
-
-    if (await client.getData({ id: message.author.id }, 'blacklist', false).bol === true) return embedResponse('Wow, al parecer te has portado mal...\n\nQuieres usarme?, pues entra [Aqui](https://discord.gg/hbSahh8)')
+    function xd(a) {
+        a.then(a => {
+            return a.bol
+        })
+    }
+    if (xd(await client.getData({ id: message.author.id }, 'blacklist'))) return embedResponse('Wow, al parecer te has portado mal...\n\nQuieres usarme?, pues entra [Aqui](https://discord.gg/hbSahh8)')
 
     /*const blacklist = []
     if (blacklist.includes(message.author.id)) return;*/
