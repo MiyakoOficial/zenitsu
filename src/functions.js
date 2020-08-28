99cbb2ce6973636611e953e005670f89e9659d91
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-08-28 18:44:36 -0400

    rank update

--- src/functions.js
+++ src/functions.js
@@ -2,7 +2,11 @@ function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 };
 
+function rModel(modelo) {
+    return require(`./models/${modelo}.js`)
+}
 
 module.exports = {
-    capitalize
+    capitalize,
+    rModel
 }
