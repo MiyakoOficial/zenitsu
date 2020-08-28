931a402de159ecf8945295cce543d58c43298f5e
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-08-28 13:01:49 -0400

    rank update

--- src/server.js
+++ src/server.js
@@ -1047,13 +1047,22 @@ client.on('message', async (message) => {
             if (res.length === 0) return embedResponse("No hay datos...").catch(err => { enviarError(err, message.author) });
 
             let pagina = res.slice(10 * (seleccion - 1), 10 * seleccion);
-            embedResponse(pagina.map((v, i) =>
 
-                `${i + 1} | ${!client.users.cache.get(v.idMember) ? 'Miembro desconocido!' : client.users.cache.get(v.idMember).tag} - ${!v.nivel ? 0 : v.nivel}`
+            let embed = Discord.MessageEmbed()
+                .setDescription(
+                    pagina.map((v, i) =>
+
+                        `${i + 1} | ${!client.users.cache.get(v.idMember) ? 'Miembro desconocido!' : client.users.cache.get(v.idMember).tag} - ${!v.nivel ? 0 : v.nivel}`
 
 
 
-            ).join('\n') || 'Pagina inexistente!').catch(err => { enviarError(err, message.author) });
+                    ).join('\n') || 'Pagina inexistente!'
+                )
+                .setTimestamp
+                .setFooter(`Pagina actual: ${seleccion === 0 ? 1 : seleccion}`)
+                .setColor(color)
+
+            message.channel.send({ embed: embed }).catch(err => { enviarError(err, message.author) });
 
         });
 
