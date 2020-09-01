42b74acc59cc3b1e8d4b34854dc2376590a55eda
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-09-01 15:19:12 -0400

    new update

--- src/server.js
+++ src/server.js
@@ -1851,12 +1851,12 @@ client.on('guildMemberUpdate', async (oldUser, newUser) => {
 client.on('message', async (msg) => {
     if (msg.channel.type === 'dm') return;
 
-    let args = message.content.split(' ');
-    `[Link of the message](https://discordapp.com/channels/${newMessage.guild.id}/${newMessage.channel.id}/${newMessage.id})`
+    let args = msg.content.split(' ');
+    //`[Link of the message](https://discordapp.com/channels/${newMessage.guild.id}/${newMessage.channel.id}/${newMessage.id})`
     let res = args[0];
     let splited = res.split('/')
-    if (message.channel.messages.fetch(splited[5])) {
-        message.channel.send('Fetched: ' + await message.channel.messages.fetch(splited[6]).then(a => a.content))
+    if (msg.channel.messages.fetch(splited[5])) {
+        msg.channel.send('Fetched: ' + await msg.channel.messages.fetch(splited[6]).then(a => a.content))
     }
 
     msg.channel.messages.fetch({ limit: 3 }).then(m => {
