c4af768b5397d11f1a6bdffb04d746401988bdaa
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-09-04 14:39:07 -0400

    accept cmd

--- src/server.js
+++ src/server.js
@@ -614,7 +614,7 @@ client.on('message', async (message) => {
         if (!args[1]) return embedResponse('Escribe algo!').catch(error => { enviarError(error, message.author) });
 
         let canalFetch = await client.channels.cache.get('727948582556270682').messages.fetch(args[0])
-        if (!canalFetch) return embedResponse('No encontre ese mensaje').catch(error => { enviarError(error, message.author) });
+        if (!canalFetch.then(a => a)) return embedResponse('No encontre ese mensaje').catch(error => { enviarError(error, message.author) });
         else {
             canalFetch.then(a => {
                 a.edit(a.embeds[0]
