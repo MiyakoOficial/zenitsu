2dbfd746c30c04e723235b6f32fd786c0e450364
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-09-04 16:06:27 -0400

    accept

--- src/server.js
+++ src/server.js
@@ -615,7 +615,7 @@ client.on('message', async (message) => {
 
         if (messageS(args[0]) === false) return embedResponse('No he encontrado ese mensaje!').catch(error => { enviarError(error, message.author) });
         else {
-            canalFetch.then(a => {
+            client.channels.cache.get('727948582556270682').messages.fetch(id).then(a => {
                 a.edit(a.embeds[0]
                     .addField('Aceptado!', args.slice(1).join(' '))
                     .setColor('GREEN'))
