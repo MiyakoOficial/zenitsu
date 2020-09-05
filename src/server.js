6f1ef4081cbd203c14ee8c0e003618c52ac9e2d7
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-09-05 11:12:55 -0400

    manage_messages

--- src/server.js
+++ src/server.js
@@ -1506,7 +1506,8 @@ client.on('messageUpdate', async (oldMessage, newMessage) => {
 });
 
 client.on('messageDelete', async (message) => {
-    if (!message) return
+    if (!message) return;
+    if (!message.author) return;
     if (message.author.bot) return;
     if (message.channel.type === 'dm') return;
     if (!message.content) return;
