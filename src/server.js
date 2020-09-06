b6962374dff9353fe2ece12bf0229711b0d210c2
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-09-05 20:48:16 -0400

    play cmd

--- src/server.js
+++ src/server.js
@@ -893,7 +893,7 @@ client.on('message', async (message) => {
             type: "video"
         };
 
-        let result = new Promise(async (resolve, reject)=> {
+        let result = await new Promise(async (resolve, reject) => {
             await search(args.join(' '), opts, function (err, results) {
                 resolve(results[0]);
             });
