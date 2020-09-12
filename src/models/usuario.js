1fff6e6cc9dbd4025acc6a6e667b7760dde801a2
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-09-12 00:58:46 -0400

    xd

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -3,7 +3,8 @@ const { Schema, model } = require('mongoose');
 const Guild = Schema({
     id: { type: String, default: 'none' },
     tokenChat: { type: String, default: 'none' },
-    grupos: Array
+    grupos: Array,
+    unidos: Array
 
 });
 module.exports = model('usuariosChat', Guild)
