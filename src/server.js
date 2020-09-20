ce6e5dbb5c37d76ce519b6f9f025047e849df77e
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-09-20 17:52:06 -0400

    LIVE

--- src/server.js
+++ src/server.js
@@ -2057,7 +2057,7 @@ client.on('message', async (message) => {
     else if (command === 'reanimador') {
         if (message.guild.id !== '757067889550557205')
             return;
-        let arraySick = ['El paciente revivió', 'El paciento murió'];
+        let arraySick = ['El paciente revivió', 'El paciente murió'];
         let res = arraySick[Math.floor(Math.random() * arraySick.length)];
         embedResponse(`${capitalize(res)}`).catch(err => { enviarError(err, message.author) });
     }
