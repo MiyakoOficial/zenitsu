00f9014013ae8672a6d0fa580d6292ac80922c59
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-10-13 13:06:16 -0400

    poniendo comandos de diversion

--- cmds/diversion/baka.js
+++ cmds/diversion/baka.js
@@ -1,19 +1,22 @@
 const Discord = require('discord.js');
 module.exports = {
     config: {
-        name: "smug",//Nombre del cmd
+        name: "baka",//Nombre del cmd
         alias: [], //Alias
-        description: "Manda un gif de presumido", //Descripción (OPCIONAL)
-        usage: "z!smug",
+        description: "Manda un gif de la expresion 'baka'", //Descripción (OPCIONAL)
+        usage: "z!baka @mencion",
         category: 'diversion'
 
     }, run: async ({ client, message, args, embedResponse, Hora }) => {
 
-        let link = (await client.neko.smug()).url
+        let link = (await client.neko.baka()).url
+        let miembro = message.mentions.members.first();
+
+        if (!miembro) return embedResponse('Tienes que mencionar a alguien!')
 
         let embed = new Discord.MessageEmbed()
             .setColor(client.color)
-            .setDescription(`${message.author.tag} se ha puesto a presumir!`)
+            .setDescription(`>-< ${miembro.user.toString()} baka!`)
             .setImage(link)
             .setTimestamp()
 
