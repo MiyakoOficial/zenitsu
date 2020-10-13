e08e887aab0e1a0d9af9493f702a72f8cb4257a2
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-10-13 13:17:19 -0400

    poniendo comandos de diversion

---
+++ cmds/diversion/slap.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+module.exports = {
+    config: {
+        name: "slap",//Nombre del cmd
+        alias: [], //Alias
+        description: "Manda un gif golpeando a alguien", //Descripción (OPCIONAL)
+        usage: "z!slap @mencion",
+        category: 'diversion'
+
+    }, run: async ({ client, message, args, embedResponse, Hora }) => {
+
+        let link = (await client.neko.slap()).url
+
+        let miembro = message.mentions.members.first();
+
+        if (!miembro) return embedResponse('Tienes que mencionar a alguien!')
+
+        let embed = new Discord.MessageEmbed()
+            .setColor(client.color)
+            .setDescription(`${message.author.toString()} golpeó a ${miembro.user.toString()}!`)
+            .setImage(link)
+            .setTimestamp()
+
+        return message.channel.send({ embed: embed }).catch(er => { });
+
+    }
+}
