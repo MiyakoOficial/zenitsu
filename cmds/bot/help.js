7bbf29e7355930ed1c4e86ea9ca06a48ae338d13
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-10-13 11:57:30 -0400

    xd

--- cmds/bot/help.js
+++ cmds/bot/help.js
@@ -22,6 +22,7 @@ module.exports = {
             .addField('Bot', client.commands.filter(a => a.config.category === 'bot').map(a => `\`${a.config.name}\``).join(', '))
             .addField('Among Us', client.commands.filter(a => a.config.category === 'among us').map(a => `\`${a.config.name}\``).join(', '))
             .addField('Administracion', client.commands.filter(a => a.config.category === 'administracion').map(a => `\`${a.config.name}\``).join(', '))
+            .addField('Developer', client.commands.filter(a => a.config.category === 'developer').map(a => `\`${a.config.name}\``).join(', '))
 
         if (!args[0])
             return message.channel.send({ embed: embedHelp });
