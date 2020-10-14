53fcf4bec3f12221e78a57b37b8e4cbed0a5a174
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-10-14 14:35:05 -0400

    guild

--- events/cliente/guildDelete.js
+++ events/cliente/guildDelete.js
@@ -2,18 +2,24 @@ const Discord = require('discord.js');
 
 module.exports = async (client, guild) => {
 
-    let add1 = `
+    let add1 = `\`\`\`diff
 - Nombre: ${guild.name}
 - Dueño: ${guild.owner.user.tag}
-Server ID: ${guild.id}
-Miembros: ${guild.members.cache.size}(Humanos: ${guild.members.cache.filter(a => !a.user.bot).size})
+- Server ID: ${guild.id}
+- Miembros: ${guild.members.cache.size}(Humanos: ${guild.members.cache.filter(a => !a.user.bot).size})\`\`\`
 `
+
+    let add2 = `\`\`\`diff
+    - Servidores: ${client.guilds.cache.size}
+    - Usuarios: ${client.users.cache.size}
+    - Canales: ${client.channels.cache.size}\`\`\``
 
     let embed = new Discord.MessageEmbed()
         .setColor('#FF0000')
         .setThumbnail(guild.iconURL)
         .setTimestamp()
         .addField('Información del servidor.', add1)
+        .addField('Estadisticas.', add2)
 
     guild.channels.cache.random().send({ embed: embed })
 };
