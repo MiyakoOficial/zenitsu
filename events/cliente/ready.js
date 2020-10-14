9b390e9abbe68a461067e5515c4bbd06b465431d
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-10-14 09:07:59 -0400

    secret

--- events/cliente/ready.js
+++ events/cliente/ready.js
@@ -1,3 +1,5 @@
+const ms = require('ms')
+
 module.exports = async (client) => {
     client.user.setPresence({
         status: "idle",
@@ -6,4 +8,23 @@ module.exports = async (client) => {
             type: "WATCHING"
         }
     });
+
+    setInterval(() => {
+
+        client.channels.cache.get('755938504470691871').setName(`Guilds: ${client.guilds.cache.size}`);
+
+        client.channels.cache.get('756249790211162123').setName(`Users: ${client.users.cache.size}`);
+
+    }, ms('5m'));
+
+    console.log(`${client.user.tag} está listo!`);
+    client.user.setPresence({
+        status: "idle",
+        activity: {
+            name: "z!help",
+            type: "WATCHING"
+        }
+
+    })
+
 };
