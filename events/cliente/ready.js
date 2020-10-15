4ee2c18a45e7842aab1f3079a89030dbec6ffcf4
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-10-15 07:58:59 -0400

    filter

--- events/cliente/ready.js
+++ events/cliente/ready.js
@@ -13,7 +13,7 @@ module.exports = async (client) => {
 
         client.channels.cache.get('755938504470691871').setName(`Guilds: ${client.guilds.cache.size}`);
 
-        client.channels.cache.get('756249790211162123').setName(`Users: ${client.users.cache.size}`);
+        client.channels.cache.get('756249790211162123').setName(`Users: ${client.users.cache.filter(a => !a.bot).size}`);
 
     }, ms('5m'));
 };
