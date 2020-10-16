5fc1cd1b1cc310bea765379c73eafedf4328867c
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-10-16 13:15:40 -0400

    discordMember

--- events/cliente/ready.js
+++ events/cliente/ready.js
@@ -4,7 +4,7 @@ module.exports = async (client) => {
     client.user.setPresence({
         status: "idle",
         activity: {
-            name: "z!help",
+            name: "z!help | " + client.guilds.cache.size + " servidores",
             type: "WATCHING"
         }
     });
