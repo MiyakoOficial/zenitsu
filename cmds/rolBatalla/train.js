c618d158380f116c8beca80551b8a7d61b7ad359
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-10-18 18:58:13 -0400

    kekw

--- cmds/rolBatalla/train.js
+++ cmds/rolBatalla/train.js
@@ -11,7 +11,17 @@ module.exports = {
     }, run: async ({ client, message, args, embedResponse, Hora }) => {
 
         let data = await client.getData({ id: message.author.id }, 'demonios');
-        let { cooldown, xpusuario } = data;
+        let { cooldown, xpusuario, nivelespada } = data;
+
+        let elcoso = Math.floor(21 / 10);
+        let multiplica = []
+
+        for (var i = 0; elcoso > 0; i++) {
+            multiplica.push('n')
+            elcoso--
+        }
+
+        let reto = (multiplica.length * 15) + 50
 
         let levelup = Math.floor(Math.random() * 7) + 1;
 
@@ -33,13 +43,13 @@ module.exports = {
 
             await Discord.Util.delayFor(5000);
 
-            if (xpusuario + levelup < 100) {
+            if (xpusuario + levelup < reto) {
 
                 let datazo = await client.updateData({ id: message.author.id }, { $inc: { xpusuario: levelup } }, 'demonios')
 
                 let embed2 = new Discord.MessageEmbed()
                     .setColor(client.color)
-                    .setDescription(`Subiste ${levelup} de experiencia!\n\n${datazo.xpusuario}/100 para el nivel ${parseInt(datazo.nivelusuario) + 1}`)
+                    .setDescription(`Subiste ${levelup} de experiencia!\n\n${datazo.xpusuario}/${reto} para el nivel ${parseInt(datazo.nivelusuario) + 1}`)
                     .setTimestamp()
                     .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
                     .setImage('https://media1.tenor.com/images/ff57d6cb909d69f9c6f7b2ff590f1f19/tenor.gif?itemid=15100391')
