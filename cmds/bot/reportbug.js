c28f0c9560b5bebbb97f2ddd60514ee1d4f3f187
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-10-18 18:17:02 -0400

    top

--- cmds/bot/reportbug.js
+++ cmds/bot/reportbug.js
@@ -11,7 +11,7 @@ module.exports = {
     run: async ({ client, message, args, embedResponse, Hora }) => {
 
         if (!args[0]) return embedResponse('Escribe algo!')
-        return embedResponse(`${message.author.tag} reportó:\n\n${args.join(' ')}`.slice(0, 2048), client.channels.cache.get('725053091522805787')).then(a => {
+        return embedResponse(`${message.author.tag}(${message.author.id}) reportó:\n\n${args.join(' ')}`.slice(0, 2048), client.channels.cache.get('725053091522805787')).then(a => {
             return embedResponse('Reporte enviado!');
         })
     }
