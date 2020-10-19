326b05735ee4fb691199bca2b191e89d40ce1622
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-10-19 17:54:42 -0400

    top update

--- cmds/rolBatalla/top.js
+++ cmds/rolBatalla/top.js
@@ -12,26 +12,59 @@ module.exports = {
 
         const { color } = client;
 
-        await client.rModel('demonios').find().limit(150).sort({ nivelespada: -1 }).exec(async (err, res) => {
-            if (err) return console.log(err);
-            if (res.length === 0) return embedResponse("No hay datos...")
+        switch (args[0]) {
+            case 'level': await client.rModel('demonios').find().limit(150).sort({ nivelespada: -1 }).exec(async (err, res) => {
+                if (err) return console.log(err);
+                if (res.length === 0) return embedResponse("No hay datos...")
 
-            let pagina = res.slice(10 * (seleccion - 1), 10 * seleccion);
+                let pagina = res.slice(10 * (seleccion - 1), 10 * seleccion);
 
-            let embed = new Discord.MessageEmbed()
-                .setDescription(
-                    pagina.map((v, i) =>
+                let embed = new Discord.MessageEmbed()
+                    .setDescription(
+                        pagina.map((v, i) =>
 
-                        `${(i + 1) + 10 * (seleccion <= 0 ? 1 : seleccion - 1)} | ${!client.users.cache.get(v.id) ? 'Miembro desconocido!' : client.users.cache.get(v.id).tag} - ${!v.nivelespada ? 0 : v.nivelespada}`
+                            `${(i + 1) + 10 * (seleccion <= 0 ? 1 : seleccion - 1)} | ${!client.users.cache.get(v.id) ? 'Miembro desconocido!' : client.users.cache.get(v.id).tag} - ${!v.nivelespada ? 0 : v.nivelespada}`
 
-                    ).join('\n') || 'Pagina inexistente!'
-                )
-                .setTimestamp()
-                .setFooter(`Pagina actual: ${seleccion <= 0 ? 1 : seleccion}`)
-                .setColor(color)
+                        ).join('\n') || 'Pagina inexistente!'
+                    )
+                    .setTimestamp()
+                    .setFooter(`Pagina actual: ${seleccion <= 0 ? 1 : seleccion}`)
+                    .setColor(color)
 
-            message.channel.send({ embed: embed }).catch(e => { })
+                message.channel.send({ embed: embed }).catch(e => { })
 
-        });
+            });
+                break;
+
+            case 'kill': await client.rModel('demonios').find().limit(150).sort({ monstruos: -1 }).exec(async (err, res) => {
+                if (err) return console.log(err);
+                if (res.length === 0) return embedResponse("No hay datos...")
+
+                let pagina = res.slice(10 * (seleccion - 1), 10 * seleccion);
+
+                let embed = new Discord.MessageEmbed()
+                    .setDescription(
+                        pagina.map((v, i) =>
+
+                            `${(i + 1) + 10 * (seleccion <= 0 ? 1 : seleccion - 1)} | ${!client.users.cache.get(v.id) ? 'Miembro desconocido!' : client.users.cache.get(v.id).tag} - ${!v.monstruos ? 0 : v.monstruos}`
+
+                        ).join('\n') || 'Pagina inexistente!'
+                    )
+                    .setTimestamp()
+                    .setFooter(`Pagina actual: ${seleccion <= 0 ? 1 : seleccion}`)
+                    .setColor(color)
+
+                message.channel.send({ embed: embed }).catch(e => { })
+
+            });
+                break;
+
+            default:
+
+                return embedResponse(`Elije entre las opciones: level, kill`)
+
+                break;
+
+        }
     }
 }
