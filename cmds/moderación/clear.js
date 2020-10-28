ba0b69cb879656b4ea3eb5c811e152f60bf5e053
MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>
2020-10-28 16:03:31 -0400

    Update clear.js

--- cmds/moderación/clear.js
+++ cmds/moderación/clear.js
@@ -11,7 +11,7 @@ module.exports = {
         if (!message.member.hasPermission('MANAGE_MESSAGES')) return embedResponse('No tienes el permiso `MANAGE_MESSAGES`!')
         if (!message.guild.me.hasPermission('MANAGE_MESSAGES')) return embedResponse('No tengo el permiso `MANAGE_MESSAGES`')
         if (!args[0]) return embedResponse('Escribe un numero!')
-        if (isNaN(args[0])) return embedResponse('' + mal + ' Escribe un numero!')
+        if (isNaN(args[0])) return embedResponse('Escribe un numero!')
         if (args[0] >= 100 || args[0] === 0) return embedResponse('Un numero del 1 al 99')
         await message.delete()
         await message.channel.bulkDelete(args[0], true).then(d => {
