d12b18ad6db2874645a3389457ff11e020cdbac3
MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>
2020-11-03 15:27:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ const client = new Discord.Client(
     }
 );
 
+const moment = require('moment');
+moment.updateLocale('es', {
+ months: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split('_'),
+ monthsShort: 'Enero._Feb._Mar_Abr._May_Jun_Jul._Ago_Sept._Oct._Nov._Dec.'.split('_'),
+ weekdays: 'Domingo_Lunes_Martes_Miercoles_Jueves_Viernes_Sabado'.split('_'),
+ weekdaysShort: 'Dom._Lun._Mar._Mier._Jue._Vier._Sab.'.split('_'),
+ weekdaysMin: 'Do_Lu_Ma_Mi_Ju_Vi_Sa'.split('_')
+}
+);
+moment.locale("es");
+
 client.neko = new nekos().sfw;
 client.star = require('star-labs');
 client.queue = new Map();
