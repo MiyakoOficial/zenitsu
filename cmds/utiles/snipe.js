ec0ff3cd1ff06582c8d5ce1b07d2adb1562aaa9f
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-11-08 10:27:29 -0400

    Xd

--- cmds/utiles/snipe.js
+++ cmds/utiles/snipe.js
@@ -19,17 +19,21 @@ module.exports = {
 
         if (!data || !data.snipe || data.snipe === 'default') return embedResponse("Nada en la base de datos")
         else {
+
+
             let la_data = data.snipe
             let separador = la_data.split(ayuda)
-            if (!separador || !separador[1]) return embedResponse("Nada en la base de datos")
+            if (!separador || !separador[1]) return embedResponse("Nadie ha borrado un mensaje en el canal.")
+
             let embed = new Discord.MessageEmbed()
-                .addField('Message', separador[0].slice(0, 1024).includes('discord.gg/') ? `The message has an invite!` : separador[0].slice(0, 1024))
-                .addField('Author', separador[1])
+                .addField('Mensaje', '\u200b')
+                .setDescription(separador[0])
+                .addField('Autor', separador[1])
                 .setColor(client.color)
                 .setTimestamp()
                 .setTitle('Snipe')
                 .setThumbnail('https://media1.tenor.com/images/8c3e8a0a3c7b0afc22624c9278be6a89/tenor.gif?itemid=5489827')
-            return message.channel.send({ embed: embed })
+            return message.channel.send({ embed: embed }).catch(e => { })
         }
     }
 }
