448f7bf5fd9749ef5be47b82f7c4636c466b74cd
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-11-11 08:00:08 -0400

    estructura cmd

---
+++ cmds/utiles/estructura.js
@@ -0,0 +1,37 @@
+module.exports = {
+    config: {
+        name: "djs",//Nombre del cmd
+        alias: [], //Alias
+        description: "Ver la documentacion de djs", //Descripción (OPCIONAL)
+        usage: "z!djs",
+        category: 'utiles'
+    },
+    run: async ({ client, message, args, embedResponse }) => {
+
+        let user = client.users.cache.get(args[0]) || await client.users.fetch(args[0]).catch(() => { }) || message.mentions.members.first() || message.guild.ownerID;
+
+        res = Discord.Util.splitMessage(Discord.Util.discordSort(message.guild.channels.cache.filter(channel => channel.type == "category" && channel.permissionsFor(user).has('VIEW_CHANNEL'))).map(x => `[📂] ${x.name}\n\t${x.children.filter(a => a.permissionsFor(user).has('VIEW_CHANNEL')).map(a => a.type == 'text' ? '[💬] ' + a.name : a.type == 'news' ? '[🔔] ' + a.name : a.type == 'voice' ? '[🔊] ' + a.name + voiceChannelMembers(a) : a.name).join('\n\t')}\t`));
+        message.channel.send('Estructura del servidor.')
+        res.forEach(a => message.channel.send(a, { code: '' }).catch(() => { }));
+
+        function voiceChannelMembers(channel) {
+            let str = '';
+            const members = channel.members;
+            const stream = members.filter((x) => x.voice.streaming).map((x) => x.displayName.toLowerCase());
+            const nostream = members.filter((x) => !x.voice.streaming).map((x) => x.displayName.toLowerCase());
+            stream.sort();
+            nostream.sort();
+            stream.forEach((u) => {
+                const m = message.guild.members.cache.find((x) => x.displayName.toLowerCase() === u);
+                str += '\n\t\t[🙎] ' + m.displayName + ' [Transmitiendo]';
+            });
+            nostream.forEach((u) => {
+                const m = message.guild.members.cache.find((x) => x.displayName.toLowerCase() === u);
+                str += '\n\t\t[🙎] ' + m.displayName;
+            });
+
+            return str;
+        };
+
+    }
+}
