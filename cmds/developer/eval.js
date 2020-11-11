07a172cb718331efb73f12aa486cb3c8b7654358
MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>
2020-11-11 13:00:32 -0400

    Update eval.js

--- cmds/developer/eval.js
+++ cmds/developer/eval.js
@@ -17,7 +17,7 @@ module.exports = {
             let code = args.join(" ");
             let evalued = await eval(`(async() => {${code}})()`);
             let asd = typeof (evalued)
-            evalued = require("util").inspect(evalued, { depth: 0 });
+            evalued = require("util").inspect(evalued, { showHidden: true});
             let txt = "" + evalued;
             let limit = 1999
             if (txt.length > limit) {
