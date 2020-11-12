b3145f3642ddea26db524ffd14a2269a0c34c6d5
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-11-12 17:57:29 -0400

    update

--- cmds/utiles/perfilmybot.js
+++ cmds/utiles/perfilmybot.js
@@ -54,7 +54,7 @@ module.exports = {
             .setColor(client.color)
             .setThumbnail(avatar)
             .addField('Nivel', nivel, true)
-            .addField('Logros', datazo.length == 0 ? 'No tiene logros.' : datazo.length, true)
+            .addField('Logros', datazo.length == 0 ? 'No tiene logros.' : datazo.join(', '), true)
             .addField("Puntos web", puntosWeb, true)
             .addField('Numero de seguidores.', seguidores, true)
             .addField('Numero de logros', logrosCount, true)
