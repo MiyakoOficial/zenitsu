9b2236b199a71c0c8c2f5d87fc205df99500ed6e
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-11-15 10:40:42 -0400

    .

--- cmds/utiles/portalmybot.js
+++ cmds/utiles/portalmybot.js
@@ -27,7 +27,7 @@ module.exports = {
             .addField('Numero de seguidores', data.seguidores, true)
             .addField('Puntos web', data.puntosWeb, true)
             .addField('Ultimo codigo', data.codigos.length >= 1 ? `[${data.codigos[0].titulo}](${data.codigos[0].link})` : 'No tiene codigos subidos.', true)
-            .addField('Logros', data.logros.join(', '), true)
+            .addField('Logros', data.logros >= 1 ? data.logros.join(', ') : 'Sin logros.', true)
 
         message.channel.send({ embed: embed }).catch(() => { });
     }
