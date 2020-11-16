6099970b2eb52393bcadae99709e230eac471f62
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-11-16 11:06:29 -0400

    xd

--- cmds/servidor/ticket.js
+++ cmds/servidor/ticket.js
@@ -1,6 +1,3 @@
-//Después de Alias es opcional.
-const Discord = require('discord.js');
-
 module.exports = {
     config: {
         name: "ticket", //nombre del cmd
@@ -9,9 +6,34 @@ module.exports = {
         usage: "z!ticket",
         category: 'servidor'
     },
-    run: ({ client, message, args }) => {
+    run: ({ message, args, embedResponse }) => {
 
+        let canalCheck = message.guild.channels.cache.find(a => a.name === `ticket-${message.author.id}`);
 
+        if (canalCheck)
+            return embedResponse('Ya tienes un ticket creado.')
+
+        let razon = args.join(' ') || 'No especificada.'
+
+        message.guild.channels.create('ticket-' + message.author.id, {
+            parent: '777876208117350401', //zenitsu-tickets
+            permissionOverwrites: [
+                {
+                    id: message.guild.id, //servidor
+                    deny: ['VIEW_CHANNEL'],
+                },
+                {
+                    id: message.author.id, //usuario
+                    allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'ATTACH_FILES', 'EMBED_LINKS']
+                },
+                {
+                    id: '649015467591335959', //rol de soporte
+                    allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'ATTACH_FILES', 'EMBED_LINKS']
+                }
+            ],
+        }).then(a => {
+            a.send(`>>> Hola ${message.author.toString()}, espera que un staff venga a ayudarte.\nRazon de ticket: ${razon}`)
+        });
 
     }
 };
