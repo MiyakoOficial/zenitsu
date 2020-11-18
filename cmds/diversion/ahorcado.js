93d7a9de4427db8b1e17443bfbfdebb729384ae6
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-11-18 15:45:18 -0400

    xd

--- cmds/diversion/ahorcado.js
+++ cmds/diversion/ahorcado.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require("discord.js");
+const ms = require("ms");
 module.exports = {
     config: {
         name: "ahorcado", //Nombre del cmd
@@ -7,7 +8,9 @@ module.exports = {
         usage: "z!akinator",
         category: 'utiles'
     },
-    run: async ({ message }) => {
+    run: async ({ message, embedResponse }) => {
+
+        let nou = 0;
 
         message.guild.letrasdichas = [];
         message.guild.fallidas = [];
@@ -21,7 +24,7 @@ module.exports = {
         let chosed = chose[Math.floor(Math.random() * chose.length)];
 
         if (message.guild.playing)
-            return message.reply('Aun estan jugando.')
+            return embedResponse('Alguien en el servidor esta jugando.')
 
         message.guild.playing = true;
 
@@ -31,20 +34,18 @@ module.exports = {
 
         if (!aw) {
             delete message.guild.playing
-            return message.channel.send(`${mention.tag} no respondio.`)
+            return embedResponse(`${mention.tag} no ha respondido correctamente.`)
         }
 
-        message.channel.send(`Realmente eleji a ${chosed.tag} porque se ve muy facherita`)
+        await embedResponse(`Eleji a ${chosed.tag} para que elija la frase.`)
 
         let palabra = await waitWord(chosed, m => m.author.id == chosed.id)
 
-        console.log(palabra)
-
         if (palabra == 'dm') {
             delete message.guild.playing
-            return message.channel.send(`${chosed.tag} tiene los dms desactivados.`)
+            return embedResponse(`${chosed.tag} tiene los dms desactivados.`)
         } if (!palabra) {
-            message.channel.send("No se elijio la frase")
+            embedResponse("No se elijio la frase")
             return delete message.guild.playing
         }
         message.guild.frase = palabra
@@ -55,12 +56,23 @@ module.exports = {
             .setFooter(`Dichas: ${message.guild.letrasdichas.join(', ')} | Fallidas: ${message.guild.fallidas.join(', ')}`)
         let msg = await message.channel.send({ embed: embed });
 
-        const filtrador = m => m.author.id == chose.find(a => a.id != chosed.id)
+        const collector = message.channel.createMessageCollector(true);
 
-        const collector = message.channel.createMessageCollector(filtrador);
-
-        collector.on('collect', m => {
-
+        collector.on('collect', async m => {
+            nou++
+            if (m.author.id != chose.find(a => a.id != chosed.id))
+                return;
+            if (nou == 6) {
+                nou = 0;
+                msg.delete().catch(() => { })
+                msg = await message.channel.send({
+                    embed:
+                        new MessageEmbed()
+                            .setColor(`#e74c3c`)
+                            .setDescription("```" + idk(message.guild.frase, message.guild.letrasdichas) + "```")
+                            .setFooter(`Dichas: ${message.guild.letrasdichas.join(', ')} | Fallidas: ${message.guild.fallidas.join(', ')}`)
+                })
+            }
             if (m.content.length == 1 && !(/[^A-Z]/gi.test(m.content))) {
 
                 if (message.guild.letrasdichas.includes(m.content.toLowerCase()) || message.guild.fallidas.includes(m.content.toLowerCase()))
@@ -81,25 +93,23 @@ module.exports = {
                         delete message.guild.playing;
                         message.guild.perdi = true;
                         collector.stop();
-                        return message.channel.send('>>> Perdiste!\n\nLa frase era: ' + message.guild.frase)
+                        return embedResponse('Perdiste!\n\nLa frase era: ' + message.guild.frase)
                     }
                 }
-
                 if (idk(message.guild.frase, message.guild.letrasdichas) == message.guild.frase) collector.stop();
-
+                message.guild.adivinador = m.author;
                 return msg.edit({
                     embed:
                         msg.embeds[0].setFooter(`Dichas: ${message.guild.letrasdichas.join(', ')} | Fallidas: ${message.guild.fallidas.join(', ')}`)
                             .setDescription(`\`\`\`${idk(message.guild.frase, message.guild.letrasdichas)}\`\`\``)
                 })
-
             }
 
         });
 
         collector.on('end', () => {
             if (!message.guild.perdi) {
-                message.channel.send('Has ganado!')
+                embedResponse(message.guild.adivinador.tag + ' ha ganado!')
                 delete message.guild.playing
                 delete message.guild.perdi
             }
