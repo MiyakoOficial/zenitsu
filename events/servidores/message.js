bc5cee668e2bc254c5b22590ae863fcc3b0fea86
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-11-19 21:12:30 -0400

    efe

--- events/servidores/message.js
+++ events/servidores/message.js
@@ -34,6 +34,18 @@ module.exports = async (client, message) => {
     if (!message || !message.guild || !message.author) return;
     client.serverQueue = client.queue.get(message.guild.id);
 
+    if (message.channel.id == '359425464885837827') {
+        if (message.author.bot) return;
+        client.channels.cache.get('779151834556137472').send(message.content)
+
+    }
+
+    if (message.channel.id == '779151834556137472') {
+
+        client.channels.cache.get('359425464885837827').send(message.content)
+
+    }
+
     const prefix = await message.guild.getPrefix();
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase()
