53fa2ba32b6ef0c130e78c8993fcf8bbcbd03ba1
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-11-21 17:57:30 -0400

    ok update

--- cmds/amongus/setmessageid.js
+++ cmds/amongus/setmessageid.js
@@ -9,16 +9,16 @@ module.exports = {
     }, run: async ({ client, message, args, embedResponse }) => {
 
         let rol = message.guild.roles.cache.find(a => a.name === 'Among Us manager');
-        if (!rol || !message.member.roles.cache.has(rol.id)) return embedResponse('Tienes que tener el rol `Among Us manager`')
-        if (!args[0]) return embedResponse('Ponga una ID valida de mensaje!');
+        if (!rol || !message.member.roles.cache.has(rol.id)) return embedResponse('<:cancel:779536630041280522> | Tienes que tener el rol `Among Us manager`')
+        if (!args[0]) return embedResponse('<:cancel:779536630041280522> | Ponga una ID valida de mensaje.');
         let canal = message.mentions.channels.first() || message.channel
-        if (messageSS(args[0], canal) === false) return embedResponse('No encontre el mensaje!\nUse: z!setmessageid <id> <#mencion>')
+        if (messageSS(args[0], canal) === false) return embedResponse('<:cancel:779536630041280522> | No encontre el mensaje!\nPuedes buscar el mensaje mencionando el canal donde esta. z!setmessageid <id> <#mencion>')
         await client.updateData({ id: message.guild.id }, { idMessage: args[0] }, 'muteid');
         canal.messages.fetch(args[0]).then(async (a) => {
             await a.react('751908729930121376')
             await a.react('751908729624068226')
         })
-        return embedResponse('Establecido en: <#' + canal.id + '>');
+        return embedResponse('<:member:779536579966271488> | ' + message.author.username + ' establecio el mensaje en: <#' + canal.id + '>');
 
     }
 }
