1857213f5f6a8421fc0ad844fe4dd676a53e1bd8
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-11-22 14:21:55 -0400

    ok update

--- cmds/niveles/rank.js
+++ cmds/niveles/rank.js
@@ -22,7 +22,7 @@ module.exports = {
                 .setDescription(
                     pagina.map((v, i) =>
 
-                        `${(i + 1) + 10 * (seleccion <= 0 ? 1 : seleccion - 1)} | ${!client.users.cache.get(v.idMember) ? 'Miembro desconocido!' : client.users.cache.get(v.idMember).tag} - ${!v.nivel ? 0 : v.nivel}`
+                        `${(i + 1) + 10 * (seleccion <= 0 ? 1 : seleccion - 1)} | ${!client.users.cache.get(v.idMember) ? v.cacheName == 'none' ? 'Miembro desconocido.' : v.cacheName : client.users.cache.get(v.idMember).tag} - ${!v.nivel ? 0 : v.nivel}`
 
                     ).join('\n') || 'Pagina inexistente!'
                 )
