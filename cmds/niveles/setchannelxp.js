d15ad47bb0a7cc73c480d651d76643fe65e4e2ff
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-11-22 15:30:16 -0400

    ok update

--- cmds/niveles/setchannelxp.js
+++ cmds/niveles/setchannelxp.js
@@ -14,6 +14,15 @@ module.exports = {
 
         let channel = message.mentions.channels.first();
         if (!channel) return embedResponse("<:cancel:779536630041280522> | Necesitas mencionar el canal.")
+
+        let embedE = new MessageEmbed()
+            .setColor(client.color)
+            .setDescription(`<:cancel:779536630041280522> | No tengo permisos para enviar mensajes en el canal mencionado.`)
+            .setTimestamp()
+
+        if (!channel.permissionsFor(client.user).has(`SEND_MESSAGES`))
+            return message.channel.send({ embed: embedE })
+
         return client.updateData({ id: message.guild.id }, { canal: channel.id }, 'logslevel')
             .then(data => {
                 return embedResponse(`<:trustedAdmin:779695112036286474> | ${message.author.tag} establecio el canal en: <#${data.canal}>`)
