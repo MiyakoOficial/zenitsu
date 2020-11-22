c4703e370c5d1dc980c5263107be435ed4bf321f
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-11-22 11:29:33 -0400

    ok update

--- cmds/bot/suggest.js
+++ cmds/bot/suggest.js
@@ -9,12 +9,17 @@ module.exports = {
         category: 'bot'
     },
     run: ({ client, message, args, embedResponse }) => {
-        if (!args[0]) return embedResponse('Escribe algo!')
+
+        if (!args[0]) return embedResponse('<:cancel:779536630041280522> | Necesitas especificar la sugerencia.')
         let embed = new Discord.MessageEmbed()
             .setColor(client.color)
+            .setDescription(`<:reason2:779695137205911552> | ${args.join(' ')}`)
             .setTimestamp()
-            .addField(message.author.tag, args.join(' '))
-        client.channels.cache.get('727948582556270682').send({ embed: embed })
-        return embedResponse('Sugerencia enviada!');
+            .setAuthor(`${message.author.tag}(${message.author.id})`)
+            .setFooter('Enviado desde ' + message.guild.name, message.guild.iconURL({ dynamic: true, size: 2048 }))
+            .setThumbnail(message.author.displayAvatarURL({ dynamic: true, size: 2048 }))
+        return client.channels.cache.get('727948582556270682').send({ embed: embed }).then(() => {
+            return embedResponse('<:reason2:779695137205911552> | Sugerencia enviado!');
+        })
     }
 }
