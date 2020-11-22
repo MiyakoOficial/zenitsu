a7c3f55c6bfdd28684110410fdecdd3ebcbe782d
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-11-22 11:04:36 -0400

    ok update

--- cmds/bot/invite.js
+++ cmds/bot/invite.js
@@ -9,12 +9,11 @@ module.exports = {
         category: 'bot'
     },
     run: ({ client, message }) => {
-
         let link = 'https://discordapp.com/oauth2/authorize?client_id=721080193678311554&scope=bot&permissions=2146958847';
         let invitacionLink = 'https://discord.gg/hbSahh8';
         let embed = new Discord.MessageEmbed()
             .setThumbnail(client.user.displayAvatarURL({ format: 'png', size: 2048 }))
-            .setDescription(`Link de invitación del bot => [Link](${link}) \nLink de invitación al servidor de soporte => [Link](${invitacionLink})`)
+            .setDescription(`Link de invitación del bot: [Link 🤖](${link}) \nLink de invitación al servidor de soporte: [Link <:zStaffZenitsu:766436216966217729>](${invitacionLink})`)
             .setColor(client.color)
             .setFooter('Gracias por apoyar!', message.author.displayAvatarURL({ format: 'png', size: 2048 }))
             .setTimestamp()
