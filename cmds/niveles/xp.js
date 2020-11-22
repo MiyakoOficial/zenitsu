f166b7a612adfb38d4091bbffa88ca8541b9b911
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-11-22 15:09:11 -0400

    ok update

--- cmds/niveles/xp.js
+++ cmds/niveles/xp.js
@@ -23,15 +23,29 @@ module.exports = {
             });
         };
 
+        let getGlobalRank = (member) => {
+
+            return new Promise((resolve) => {
+                client.rModel('niveles').find({}).sort({ nivel: -1 }).exec((err, res) => {
+
+                    let results = res.map(a => a.idMember);
+
+                    resolve(results.findIndex(a => a === member.user.id) + 1);
+
+                });
+            });
+        };
+
         let member = message.guild.members.cache.find(a => a.user.username === args.join(' ')) || message.guild.members.cache.find(a => a.user.tag === args.join(' ')) || message.guild.members.cache.find(a => a.displayName === args.join(' ')) || message.guild.members.cache.get(args[0]) || message.mentions.members.first() || message.member
         let { nivel, xp } = await client.getData({ idGuild: `${message.guild.id}`, idMember: `${member.user.id}` }, 'niveles');
         let levelup = 5 * (nivel ** 2) + 50 * nivel + 100;
         //console.log(xp, nivel);
         message.channel.startTyping();
         let rank = await getRank(member)
+        let global = await getGlobalRank(member);
         message.channel.stopTyping();
         let embed = new Discord.MessageEmbed()
-            .setDescription(`Nivel: ${nivel ? nivel : 0} \nXp: ${xp ? xp : 0}/${levelup ? levelup : '100'}\nRank: ${rank === null ? 'Sin resultados' : rank}`)
+            .setDescription(`Nivel: ${nivel ? nivel : 0} \nXp: ${xp ? xp : 0}/${levelup ? levelup : '100'}\nRank: ${rank === null ? 'Sin resultados' : rank}\nRank global: ${global}`)
             .setColor(color)
             .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
             .setTimestamp()
