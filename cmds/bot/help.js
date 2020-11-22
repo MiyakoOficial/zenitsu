00a4008fcf934e0420817ba0dd67e4b1a8f4c037
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-11-22 15:36:02 -0400

    ok update

--- cmds/bot/help.js
+++ cmds/bot/help.js
@@ -22,9 +22,9 @@ module.exports = {
             .addField('Bot', client.commands.filter(a => a.config.category === 'bot').map(a => `\`${a.config.name}\``).join(', '))
             .addField('Among Us', client.commands.filter(a => a.config.category === 'among us').map(a => `\`${a.config.name}\``).join(', '))
             .addField('Administracion', client.commands.filter(a => a.config.category === 'administracion').map(a => `\`${a.config.name}\``).join(', '))
-            .addField('Nuevo: Batallas contra demonios', client.commands.filter(a => a.config.category === 'extra').map(a => `\`${a.config.name}\``).join(', '))
             .addField('Interacción', client.commands.filter(a => a.config.category === 'interacción').map(a => `\`${a.config.name}\``).join(', '))
             .addField('(Solo disponible en el soporte)', client.commands.filter(a => a.config.category === 'servidor').map(a => `\`${a.config.name}\``).join(', '))
+            .addField('Extras', client.commands.filter(a => a.config.category === 'extra').map(a => `\`${a.config.name}\``).join(', '))
             .setImage('https://cdn.discordapp.com/attachments/765608178540609598/766651849050292234/para_el_pibe_2.jpg')
 
         if (!args[0])
