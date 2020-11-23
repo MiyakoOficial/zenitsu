49e4c28758adc562130da09a75bc18585da7dbe3
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-11-23 07:51:49 -0400

    ok update

--- cmds/utiles/avatar.js
+++ cmds/utiles/avatar.js
@@ -23,7 +23,7 @@ module.exports = {
             .setColor(client.color)
             .setImage(avatar)
             .setFooter(`Pedido por ${message.author.tag}`, message.author.displayAvatarURL({ size: 2048, dynamic: true }))
-            .setDescription(`[Link avatar](${avatar})\n[Búscalo en google](${buscarG})`)
+            .setDescription(`<:member:779536579966271488>[Link avatar](${avatar})\n🌐[Búscalo en google](${buscarG})`)
         return message.channel.send({ embed: embed }).catch(() => { });
 
     }
