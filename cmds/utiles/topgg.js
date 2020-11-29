ea40fbcca11f98b139e91cd702c694effeab0c07
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-11-29 14:04:14 -0400

    estructura

--- cmds/utiles/topgg.js
+++ cmds/utiles/topgg.js
@@ -1,5 +1,7 @@
-module.exports = {
+const { MessageEmbed } = require('discord.js');
+const { topGG } = require('myscrapper');
 
+module.exports = {
     config: {
         name: 'topgg',
         alias: [],
@@ -8,8 +10,37 @@ module.exports = {
         botPermissions: [],
         memberPermissions: [],
     },
-    run: () => {
+    run: async ({ client, args, message, embedResponse }) => {
 
+        if (!args[0])
+            return embedResponse('¿Que quieres buscar?');
+
+        let data = await topGG(args.join(' ')) || [];
+
+        if (!data || !data[0])
+            return embedResponse('Sin resultados.')
+
+        data = data[0]
+
+        let types = {
+            bot: 'bot',
+            guild: 'servidor'
+        }
+
+        let embed = new MessageEmbed()
+            .setColor(client.color)
+            .addField('Nombre', data.name)
+            .addField('Tipo', types[data.type], true)
+
+        if (data.invite)
+            embed.addField('Invitacion', data.invite, true)
+
+        embed = embed
+            .addField('Descripcion', data.description)
+            .setThumbnail(data.icon)
+            .setTimestamp()
+
+        return message.channel.send({ embed: embed })
 
     }
 
