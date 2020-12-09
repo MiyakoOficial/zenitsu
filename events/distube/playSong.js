bd48da97a7bbead8d6ca9d2462cca553114d5b04
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-12-09 11:04:46 -0400

    :D

--- events/distube/playSong.js
+++ events/distube/playSong.js
@@ -10,7 +10,8 @@ module.exports = (client, message, queue, song) => {
         .setColor(client.color)
         .setTimestamp()
         .setThumbnail(song.thumbnail)
+        .setAuthor('Reproduciendo ahora', 'https://media1.tenor.com/images/18a006ba771a888cd82e34a84e8b5ed7/tenor.gif?itemid=11366262')
         .setFooter(song.user.tag, song.user.displayAvatarURL({ dynamic: true, size: 2048 }))
-        .setDescription(`Reproduciendo ahora: [${song.name}](${song.url}) - ${song.formattedDuration}`)
+        .setDescription(`[${song.name}](${song.url}) - ${song.formattedDuration}`)
     return message.channel.send({ embed: embed })
 }
