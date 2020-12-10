c506e236366141a05dfbaa672f3c5992199c0d18
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-12-10 15:18:33 -0400

    :D

--- cmds/music/queue.js
+++ cmds/music/queue.js
@@ -39,7 +39,7 @@ module.exports = {
 
         for (let a of queue[seleccion(args[0]) - 1]) {
             ++i
-            return `[${a.fromPlaylist ? `[<:mc_song:786660726914678834>](${await getUrl(a.fromPlaylistURL)})` : '<a:songDJ:786662120388296724>'}][${(i + 1) + 10 * (seleccion(args[0]) <= 0 ? 1 : seleccion(args[0]) - 1)}] [${a.name}](${a.url}) - ${a.formattedDuration} - ${a.user.toString()}`
+            return `[${a.fromPlaylist ? `<:mc_song:786660726914678834>` : '<a:songDJ:786662120388296724>'}][${(i + 1) + 10 * (seleccion(args[0]) <= 0 ? 1 : seleccion(args[0]) - 1)}] [${a.name}](${a.url}) - ${a.formattedDuration} - ${a.user.toString()}`
 
         }
 
