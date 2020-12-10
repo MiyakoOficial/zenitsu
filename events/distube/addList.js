7c50ebb251128eee4ddd82d9b1ed49ccd156c0f1
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-12-10 14:03:37 -0400

    :D

--- events/distube/addList.js
+++ events/distube/addList.js
@@ -11,7 +11,6 @@ module.exports = (client, message, queue, playlist, song) => {
     let embed = new Discord.MessageEmbed()
         .setColor(client.color)
         .setThumbnail(playlist.thumbnail)
-        .setAuthor(song.name, song.thumbnail, song.url)
         .setDescription(`Playlist [${playlist.name}](${playlist.url}) añadida (${playlist.songs.length} canciones).`)
         .setTimestamp()
         .setFooter(song.user.tag, song.user.displayAvatarURL({ dynamic: true, size: 2048 }))
