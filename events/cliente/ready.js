e43abe0617383b217bbec6bcb7c921ac46a64ca9
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-12-11 11:53:17 -0400

    :D

--- events/cliente/ready.js
+++ events/cliente/ready.js
@@ -11,9 +11,38 @@ module.exports = (client) => {
 
     setInterval(() => {
 
+        client.voice.connections.map(a => {
+
+
+            let members = a.channel.members.filter(a => !a.user.bot);
+
+            if (members.size == 0) {
+
+                let q = client.distube.getQueue(client.guilds.cache.get(members.array()[0].guild.id));
+
+                try {
+                    a.channel.leave()
+
+                    client.distube.emit('empty', q.songs[0].message)
+                }
+                catch (e) {
+                    return;
+                }
+            }
+
+        });
+
+    }, ms('5s'));
+
+
+    setInterval(() => {
+
+
         client.channels.cache.get('755938504470691871').setName(`Guilds: ${client.guilds.cache.size}`).catch(() => { });
 
         client.channels.cache.get('756249790211162123').setName(`Users: ${client.users.cache.filter(a => !a.bot).size}`).catch(() => { });
 
+
     }, ms('5m'));
+
 };
