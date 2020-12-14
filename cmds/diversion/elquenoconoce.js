bf3a65a53b91f0e7ac406ec9d0641aeb678228f3
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-12-14 11:46:59 -0400

    flushed

--- cmds/diversion/elquenoconoce.js
+++ cmds/diversion/elquenoconoce.js
@@ -18,7 +18,7 @@ module.exports = {
         let user = message.mentions.users.first() || null;
 
         if (!user)
-            embedResponse('<:cancel:779536630041280522> | Debes de mencionar a un usuario.')
+            return embedResponse('<:cancel:779536630041280522> | Debes de mencionar a un usuario.')
 
         const avatares = [userAvatar, user.displayAvatarURL({ format: 'png', size: 4096 })];
 
