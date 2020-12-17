9b9640918efd38cfcc2a800f40173f0d24e08619
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-12-17 19:15:16 -0400

    xd

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ client.erela = new Manager({
             .setColor(client.color)
             .setTimestamp()
             .setThumbnail(song.thumbnail)
-            .setDescription(`<:accept:779536642365063189> | Cancion *\`añadida:\`* [${client.remplazar(song.title)}](${song.uri}) - ${song.isStream ? 'LIVE' : newDate(song.duration)}`)
+            .setDescription(`<:accept:779536642365063189> | Cancion *\`añadida:\`* [${client.remplazar(song.title)}](${song.uri}) - ${song.isStream ? 'LIVE' : client.newDate(song.duration)}`)
             .setFooter(song.message.author.tag, song.message.author.displayAvatarURL({ dynamic: true, size: 2048 }))
         song.message.channel.send({ embed: embed })
 
