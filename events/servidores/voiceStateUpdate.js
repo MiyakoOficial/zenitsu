2f9d8948ee4a994d832ee64b14baf18ed93b1d88
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-12-17 16:20:21 -0400

    xd

--- events/servidores/voiceStateUpdate.js
+++ events/servidores/voiceStateUpdate.js
@@ -1,5 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+const { VoiceState } = require('discord.js');
+
+/**
+ * 
+ * @param {import('discord.js').Client} client 
+ * @param {VoiceState} oldState 
+ * @param {VoiceState} newState 
+ */
+
 module.exports = (client, oldState, newState) => {
-
-    console.log(newState)
-
+    let player = client.erela.get(newState.guild.id);
+    let canalVoz = newState.channel;
+    if (!canalVoz && player) player.destroy();
 }
