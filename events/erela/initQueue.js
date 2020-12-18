ecd813b3e5f2304c50f784fca9bdb52ebd96ac51
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-12-17 20:23:41 -0400

    xd

--- events/erela/initQueue.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-const Discord = require('discord.js');
-/**
- * 
- * @param {Discord.Client} client 
- */
-module.exports = (client, queue) => {
-    queue.autoplay = false;
-}
--- events/erela/queueEnd.js
+++ events/erela/queueEnd.js
@@ -6,10 +6,13 @@ const Discord = require('discord.js');
  * @returns {Promise<Discord.Message>}
  */
 
-module.exports = (client, message) => {
+module.exports = (client, player) => {
     let embed = new Discord.MessageEmbed()
         .setColor(client.color)
         .setTimestamp()
         .setDescription('<a:baimefui:787148055421517845> | Termine de reproducir la musica, Adios!')
-    message.channel.send({ embed: embed })
+    let canal = client.channels.cache
+        .get(player.textChannel)
+    canal ? canal.send({ embed: embed }) : null
+    player.destroy();
 }
