bfe0d7d529ad007d021c42efd0d025ef796d2f5d
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-12-18 17:42:43 -0400

    xd

--- cmds/music/np.js
+++ cmds/music/np.js
@@ -9,14 +9,10 @@ module.exports = {
         category: 'musica'
     },
     run: ({ message, client, embedResponse }) => {
-
         let data = message.guild.player();
-
         if (!data || !data.playing || !data.queue || !data.queue.current)
             return embedResponse('<:cancel:779536630041280522> | Ninguna cancion se esta reproduciendo.')
-
         let res;
-
         if (!data.queue.current.isStream) {
             const createBar = require('string-progressbar');
             let total = data.queue.current.duration / 1000;
@@ -27,17 +23,14 @@ module.exports = {
             console.log(total, current)
             res = createBar(total, current, size, line, slider)[0]
         }
-
         if (!res)
             res = 'LIVE'
-
         let embed = new MessageEmbed()
             .setColor(client.color)
             .setAuthor(data.queue.current.title, 'https://media1.tenor.com/images/869a5e483261d0b8e4f296b1152cba8e/tenor.gif?itemid=15940704', data.queue.current.uri)
             .setThumbnail(data.queue.current.thumbnail)
             .setDescription(`${`[${data.queue.current.isStream ? '<a:frog_rotate:720984862231887883>' : data.queue.current.fromPlaylist ? `<:mc_song:786660726914678834>` : '<a:songDJ:786662120388296724>'}]`}[\`${res}\`]\n${data.queue.current.isStream ? 'LIVE' : `${client.newDate(data.position) + '/' + client.newDate(data.queue.current.duration)}`}`)
             .setFooter(`Puesta por: ${data.queue.current.message.author.tag}`, data.queue.current.message.author.displayAvatarURL({ dynamic: true, size: 2048 }))
-
         message.channel.send({ embed: embed })
 
     }
