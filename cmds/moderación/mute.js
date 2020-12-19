05033e6dffdc8429e172fcee18fb01ffd9fbc1b8
MARCROCK22 <marcoscolonrosario@gmail.com>
2020-12-19 16:23:38 -0400

    si

---
+++ cmds/moderación/mute.js
@@ -0,0 +1,67 @@
+const Discord = require('discord.js');
+const easy = require('easymaty');
+
+module.exports = {
+    config: {
+        name: "mute", //nombre del cmd
+        alias: [], //Alias
+        description: "Silenciar a un miembro", //Descripción (OPCIONAL)
+        usage: "z!mute @mencion razon(opcional)",
+        category: 'moderacion',
+        botPermissions: ['ADMINISTRATOR', 'MANAGE_ROLES', 'MANAGE_CHANNELS'],
+        memberPermissions: ['MANAGE_MESSAGES', 'KICK_MEMBERS']
+
+    }, run: ({ client, message, args, embedResponse }) => {
+
+        if (message.author.id != '507367752391196682')
+            return;
+
+        let roles = message.guild.roles.cache.filter(a => !a.managed && a.editable);
+
+        let roleName = 'MUTED';
+
+        if (!roles.find(a => a.name == roleName)) {
+            embedResponse('<:cancel:779536630041280522> | Necesitas crear el rol `MUTED`\n~~¿Deseas crearlo ahora?~~')
+
+            const filter = () => true;
+            message.channel.awaitMessages(filter, { max: 1, time: require('ms')('10s'), errors: ['time'] })
+                .then(collected => {
+                    console.log(collected);
+                })
+                .catch(collected => console.log(`After a minute, only ${collected.size} out of 4 voted.`));
+
+        }
+        let miembro = message.mentions.members.first();
+
+        let razon = args.slice(1).join(' ') || 'No especificada.';
+
+        if (!miembro || miembro?.user?.bot) return embedResponse('<:cancel:779536630041280522> | Menciona a un miembro del servidor.')
+
+        if (miembro.roles.highest.comparePositionTo(message.member.roles.highest) > 0)
+            return embedResponse('<:cancel:779536630041280522> | No puedes silenciar a este usuario.')
+
+        if (miembro.roles.highest.comparePositionTo(message.guild.me.roles.highest) > 0)
+            return embedResponse('<:cancel:779536630041280522> | No puedo moderar a este usuario.')
+
+        if (!args[0].match(/<@(!)?[0-9]{18}>/g)) return embedResponse('<:cancel:779536630041280522> | La mencion tiene que ser el primer argumento.')
+
+        if (message.author.id != message.guild.ownerID) {
+            if (miembro.hasPermission('ADMINISTRATOR'))
+                return embedResponse('<:cancel:779536630041280522> | ' + miembro.toString() + ' es administrador.')
+        }
+        miembro = miembro.user;
+
+        if (miembro.id == message.author.id) return embedResponse('<:cancel:779536630041280522> | No te puedes silenciar a ti mismo.')
+
+        let embed = new Discord.MessageEmbed()
+            .setColor(client.color)
+            .setTimestamp()
+            .setTitle('<a:alarma:767497168381935638> Miembro silenciado <a:alarma:767497168381935638>')
+            .setAuthor(miembro.tag, miembro.displayAvatarURL({ dynamic: true }))
+            .addField('<:reason2:779695137205911552> Razón', razon.slice(0, 1024), true)
+            .addField('<:moderator:779536592431087619> Moderador', message.author.tag, true)
+            .addField('<:reason:779536605047554068> Advertencias para ser expulsado', razon, true)
+
+        return message.channel.send({ embed: embed }).catch(() => { });
+    }
+}
