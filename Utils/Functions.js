710b7c2a110eb56ace60c53dd24c20d291d9008e
MARCROCK22 <marcoscolonrosario@gmail.com>
2021-01-11 19:49:55 -0400

    ttt cmd

--- Utils/Functions.js
+++ Utils/Functions.js
@@ -40,6 +40,25 @@ const Canvas = require('canvas');
 
 /**
  * 
+ * @param {String} string 
+ * @param {Array} array
+ * @returns {String}
+ * @example
+ * replace('tokenn', ['token']) //[PRIVATE]n
+ */
+module.exports.replace = function (string, array) {
+
+    let res = string;
+
+    for (let i of array) {
+        res = res.split(i).join('[PRIVATE]')
+    }
+
+    return res;
+
+}
+/**
+ * 
  * @param {Array<string>} mapatest 
  * @param {Boolean} win
  * @returns {Promise<Buffer>}
