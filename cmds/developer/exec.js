3b55873143d4b7f64d630c9432654d585bfb4222
MARCROCK22 <marcoscolonrosario@gmail.com>
2021-01-18 19:52:58 -0400

    promisify

--- cmds/developer/exec.js
+++ cmds/developer/exec.js
@@ -1,33 +1,40 @@
+const { promisify } = require('util');
 module.exports = {
-    config: {
-        name: "exec",
-        alias: ['ex'],
-        description: "shell",
-        usage: "z!exec npm i",
-        category: 'developer',
-        botPermissions: [],
-        memberPermissions: []
+	config: {
+		name: "exec",
+		alias: ['ex'],
+		description: "shell",
+		usage: "z!exec npm i",
+		category: 'developer',
+		botPermissions: [],
+		memberPermissions: []
 
-    },
-    // eslint-disable-next-line no-unused-vars
-    run: ({ client, message, args, embedResponse, Hora }) => {
+	},
+	// eslint-disable-next-line no-unused-vars
+	run: async ({ message, args }) => {
 
-        if (!['507367752391196682'].includes(message.author.id))
-            return;
+		if (!['507367752391196682'].includes(message.author.id))
+			return;
 
-        if (!args[0])
-            return;
+		if (!args[0])
+			return;
 
-        try {
-            // eslint-disable-next-line no-unused-vars
-            return require('child_process').exec(args.join(' '), (err, stdout, stderr) => {
-                if (err) return message.channel.send(err, { code: '', split: { char: '', maxLength: 1900 } })
-                return message.channel.send(stdout.toString(), { code: '', split: { char: '', maxLength: 1900 } })
-            })
-        } catch (err) {
+		try {
+			// eslint-disable-next-line no-unused-vars
+			const res = await promisify(require('child_process').exec)(args.join(' '))
 
-            return message.channel.send(err, { code: 'js' }).catch(() => { })
+			if(res.stderr.length){
+				message.channel.send(res.stderr, {split: {char: '', maxLength: 1950}, code: ''})
+			}
 
-        }
-    }
+			if(res.stdout.length){
+				message.channel.send(res.stderr, {split: {char: '', maxLength: 1950}, code: ''})
+			}
+
+		} catch (err) {
+
+			return message.channel.send(err, {split: {char: '', maxLength: 1950}, code: ''});
+
+		}
+	}
 }
