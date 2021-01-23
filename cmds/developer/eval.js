ed2304687ad585b31734d00364bb1a8c338cc8b7
MARCROCK22 <marcoscolonrosario@gmail.com>
2021-01-23 12:38:49 -0400

    env

--- cmds/developer/eval.js
+++ cmds/developer/eval.js
@@ -26,7 +26,7 @@ module.exports = {
 			let evalued = await eval(`(async() => {${code}})()`);
 			let TYPE = typeof (evalued)
 			evalued = require("util").inspect(evalued, { depth: 0 });
-			evalued = replace(evalued, [client.token, process.env.MONGODB, process.env.WEBHOOKID, process.env.WEBHOOKTOKEN])
+			evalued = replace(evalued, [client.token, process.env.MONGODB, process.env.WEBHOOKID, process.env.WEBHOOKTOKEN, process.env.DBLTOKEN])
 			const res = Discord.Util.splitMessage(evalued, {char: '', maxLength: 2000});
 
 			for (let minires of res){
@@ -37,7 +37,7 @@ module.exports = {
 				.setFooter(message.author.tag, message.author.displayAvatarURL({dynamic: true, size: 2048}))
 				.addField('typeof', '```js\n'+TYPE+'\n```', true)
 				message.channel.send({embed: embed})
-				await Discord.Util.delayFor(1000)
+				await Discord.Util.delayFor(2000)
 			}
 		} catch (err) {
 			message.channel.send(err, { code: 'js' })
