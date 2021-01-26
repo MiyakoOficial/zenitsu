e815881cd581e0ba93497aed44df0699904e0aac
MARCROCK22 <marcoscolonrosario@gmail.com>
2021-01-26 10:06:33 -0400

    webhook eliminada

--- index.js
+++ index.js
@@ -391,6 +391,6 @@ process.on("unhandledRejection", e => {
 		new Discord.MessageEmbed()
 			.setColor('GREEN')
 			.setTitle('Error')
-			.setDescription(`Promesa denegada sin manejar: ${e.stack}`.slice(0, 2040))
+			.setDescription(`Promesa denegada sin manejar: ${e.stack}`.slice(0, 2000))
 			.setTimestamp())
 });
