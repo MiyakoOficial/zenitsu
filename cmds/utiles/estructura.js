c09b1e8a3baee33be898a14a4bb51a8e038f9631
MARCROCK22 <marcoscolonrosario@gmail.com>
2021-01-27 10:42:57 -0400

    estructura ahora soporta roles

--- cmds/utiles/estructura.js
+++ cmds/utiles/estructura.js
@@ -2,95 +2,99 @@ const Discord = require("discord.js")
 
 const Command = require('../../Utils/Classes').Command;
 module.exports = class Comando extends Command {
-    constructor() {
-        super()
-        this.name = "estructura"
-        this.category = 'utiles'
-    }
-    async run({ message, args }) {
+	constructor() {
+		super()
+		this.name = "estructura"
+		this.category = 'utiles'
+	}
+	async run({ message, args }) {
 
-        let todo = '';
-        async function GETOWNER() {
-            return await message.guild.members.fetch(message.guild.ownerID)
-        }
-        let memberXD = args[0] == 'guild' ? await GETOWNER().catch(() => { }) : message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(a => a.displayName == args.join(' ') || a.user.tag == args.join(' ') || a.user.username == args.join(' ')) || message.mentions.members.first() || message.member;
-        if (!memberXD) memberXD = message.member
-        let printT = message.guild.channels.cache.filter(a => a.type == 'category').sort((a, b) => a.position - b.position);
+		let todo = '';
+		async function GETOWNER() {
+			return await message.guild.members.fetch(message.guild.ownerID)
+		}
+		let memberXD = args[0] == 'guild' ? await GETOWNER().catch(() => { }) : message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(a => a.displayName == args.join(' ') || a.user.tag == args.join(' ') || a.user.username == args.join(' ')) || message.mentions.members.first()
+		|| message.mentions.roles.first()
+		|| message.guild.roles.cache.get(args[0])
+		|| message.guild.roles.cache.find(r => r.name === args.join(' '))
+		message.member;
+		if (!memberXD) memberXD = message.member
+		let printT = message.guild.channels.cache.filter(a => a.type == 'category').sort((a, b) => a.position - b.position);
 
-        let without = message.guild.channels.cache.filter(a => !a.parent && a.type != 'category')
+		let without = message.guild.channels.cache.filter(a => !a.parent && a.type != 'category')
 
-        let textos = without.sort((a, b) => a.position - b.position).filter(a => a.type != 'voice').filter(a => a.permissionsFor(memberXD).has('VIEW_CHANNEL'))
-        let voz = without.sort((a, b) => a.position - b.position).filter(a => a.type == 'voice').filter(a => a.permissionsFor(memberXD).has('VIEW_CHANNEL'))
-        todo += `[${voz.size + textos.size}/${without.size}]\n`
-        textos = textos.map(a => `\t${name(a)}`)
-        voz = voz.map(a => `${textos.length >= 1 ? '\n' : ''}\t[🔊] ${a.name}${membersInfoInChannel(a)}`)
+		let textos = without.sort((a, b) => a.position - b.position).filter(a => a.type != 'voice').filter(a => a.permissionsFor(memberXD).has('VIEW_CHANNEL'))
+		let voz = without.sort((a, b) => a.position - b.position).filter(a => a.type == 'voice').filter(a => a.permissionsFor(memberXD).has('VIEW_CHANNEL'))
+		todo += `[${voz.size + textos.size}/${without.size}]\n`
+		textos = textos.map(a => `\t${name(a)}`)
+		voz = voz.map(a => `${textos.length >= 1 ? '\n' : ''}\t[🔊] ${a.name}${membersInfoInChannel(a)}`)
 
-        todo += `${textos.join('\n')}`
-        todo += `${voz.join('\n')}`;
+		todo += `${textos.join('\n')}`
+		todo += `${voz.join('\n')}`;
 
-        printT = printT.map(cat => {
+		printT = printT.map(cat => {
 
-            let canales_no_voice = cat.children.filter(a => a.permissionsFor(memberXD).has('VIEW_CHANNEL')).filter(a => a.type != 'voice')
-            let canales_si_voice = cat.children.filter(a => a.permissionsFor(memberXD).has('VIEW_CHANNEL')).filter(a => a.type == 'voice')
-            if (canales_no_voice.size == 0 && canales_si_voice.size == 0)
-                return undefined;
-            return `[📁] ${cat.name} ${canales_no_voice.sort((a, b) => a.position - b.position).map(a => `\n\t${name(a)}`).join('')}${canales_si_voice.sort((a, b) => a.position - b.position).map(a => `\n\t[🔊] ${a.name}${membersInfoInChannel(a)}`).join('')}\n`
+			let canales_no_voice = cat.children.filter(a => a.permissionsFor(memberXD).has('VIEW_CHANNEL')).filter(a => a.type != 'voice')
+			let canales_si_voice = cat.children.filter(a => a.permissionsFor(memberXD).has('VIEW_CHANNEL')).filter(a => a.type == 'voice')
+			if (canales_no_voice.size == 0 && canales_si_voice.size == 0)
+				return undefined;
+			return `[📁] ${cat.name} ${canales_no_voice.sort((a, b) => a.position - b.position).map(a => `\n\t${name(a)}`).join('')}${canales_si_voice.sort((a, b) => a.position - b.position).map(a => `\n\t[🔊] ${a.name}${membersInfoInChannel(a)}`).join('')}\n`
 
-        }).filter(a => a)
+		}).filter(a => a)
 
-        todo += `\n${printT.join('')}`
+		todo += `\n${printT.join('')}`
 
-        let res = Discord.Util.splitMessage(todo, { maxLength: 1900 });
+		let res = Discord.Util.splitMessage(todo, { maxLength: 1900 });
 
-        await message.channel.send(`**Estructura de ${memberXD.user.tag}**`).catch(() => { })
-        res.forEach(async a => await message.channel.send(a, { code: '' }).catch(() => { }))
+		await message.channel.send(`**Estructura de ${memberXD?.user?.tag ||memberXD.name}** [${(memberXD instanceof Discord.GuildMember) ? 'miembro' : 'rol'}]`).catch(() => { })
+		res.forEach(async a => await message.channel.send(a, { code: '' }).catch(() => { }))
 
-        function membersInfoInChannel(channel) {
+		function membersInfoInChannel(channel) {
 
-            let str = '';
+			let str = '';
 
-            let streaming = sortMembers(channel.members.filter(a => a.voice.streaming));
-            streaming = streaming.map(member => member.user.bot ? `\n\t\t[${emojisVoice(member, '🎧🤖', '🤖')}] ${member.displayName} [EN DIRECTO]` : `\n\t\t[${emojisVoice(member, '🧏', '🙎')}] ${member.displayName} [EN DIRECTO]`)
-            streaming.forEach(a => {
+			let streaming = sortMembers(channel.members.filter(a => a.voice.streaming));
+			streaming = streaming.map(member => member.user.bot ? `\n\t\t[${emojisVoice(member, '🎧🤖', '🤖')}] ${member.displayName} [EN DIRECTO]` : `\n\t\t[${emojisVoice(member, '🧏', '🙎')}] ${member.displayName} [EN DIRECTO]`)
+			streaming.forEach(a => {
 
-                str += a
+				str += a
 
-            });
+			});
 
-            let noStreaming = sortMembers(channel.members.filter(a => !a.voice.streaming));
-            noStreaming = noStreaming.map(member => member.user.bot ? `\n\t\t[${emojisVoice(member, "🎧🤖", '🤖')}] ${member.displayName}` : `\n\t\t[${emojisVoice(member, '🧏', '🙎')}] ${member.displayName}`)
-            noStreaming.forEach(a => {
-                str += a
-            });
-            return str
+			let noStreaming = sortMembers(channel.members.filter(a => !a.voice.streaming));
+			noStreaming = noStreaming.map(member => member.user.bot ? `\n\t\t[${emojisVoice(member, "🎧🤖", '🤖')}] ${member.displayName}` : `\n\t\t[${emojisVoice(member, '🧏', '🙎')}] ${member.displayName}`)
+			noStreaming.forEach(a => {
+				str += a
+			});
+			return str
 
-        }
-    }
+		}
+	}
 }
 
 function name(a) {
 
-    return a.guild.systemChannelID == a.id ? `[📥] ${a.name}` : a.guild.rulesChannelID == a.id ? `[📕] ${a.name}` : a.type == 'text' ? `[💬] ${a.name}` : a.type == 'news' ? `[🔔] ${a.name}` : a.type == 'store' ? `[🏬] ${a.name}` : `[❓] ${a.name}`;
+	return a.guild.systemChannelID == a.id ? `[📥] ${a.name}` : a.guild.rulesChannelID == a.id ? `[📕] ${a.name}` : a.type == 'text' ? `[💬] ${a.name}` : a.type == 'news' ? `[🔔] ${a.name}` : a.type == 'store' ? `[🏬] ${a.name}` : `[❓] ${a.name}`;
 
 }
 
 function emojisVoice(member, deaf, normal) {
 
-    return `${member.voice.selfMute || member.voice.serverMute ? '🔇' : ''}${member.voice.selfVideo ? '🎥' : ''}${member.voice.selfDeaf || member.voice.serverDeaf ? deaf : normal}`
+	return `${member.voice.selfMute || member.voice.serverMute ? '🔇' : ''}${member.voice.selfVideo ? '🎥' : ''}${member.voice.selfDeaf || member.voice.serverDeaf ? deaf : normal}`
 
 }
 
 function sortMembers(members) {
 
-    let items = members.array()
+	let items = members.array()
 
-    return items.sort(function (a, b) {
-        if (a.displayName > b.displayName) {
-            return 1;
-        }
-        if (a.displayName < b.displayName) {
-            return -1;
-        }
-        return 0;
-    });
+	return items.sort(function (a, b) {
+		if (a.displayName > b.displayName) {
+			return 1;
+		}
+		if (a.displayName < b.displayName) {
+			return -1;
+		}
+		return 0;
+	});
 }
