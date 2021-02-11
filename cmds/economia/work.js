ffc4d89954988b5f83cf426e3527ebd38aa9498e
MARCROCK22 <marcoscolonrosario@gmail.com>
2021-02-11 11:02:27 -0400

    nerfeo a las mascotas

--- cmds/economia/work.js
+++ cmds/economia/work.js
@@ -37,7 +37,8 @@ const Discord = require('discord.js'),
         'Ayudaste a AndreMor mejorando errores en su Github, te premia con: **{MONEY}**.',
         '\\*Inserte frase acá, te dio: **{MONEY}**\\*.',
         'Leri quiere tener un frase, a cambio te dio **{MONEY}**.',
-        'Trabajaste para himura, te dio: **{MONEY}**.'
+        'Trabajaste para himura, te dio: **{MONEY}**.',
+        'Ilias te pago para que seas su esclavo, te dio: **{MONEY}**'
     ];
 
 function generarFrase() {
@@ -81,7 +82,7 @@ module.exports = class Comando extends Command {
             },
             chance = generarNumero(0, 100);
         if (chances[hability] >= chance) {
-            total += Math.round((coins * hability) / 8)
+            total += Math.round((coins * hability) / 14)
             res.footerText = `¡Tu mascota ${name} ha cazado!, Toma ${total - coins}💰 más.`
         }
         await economy_model.updateOne({ id: message.author.id }, { $inc: { money: total }, $set: { cacheName: message.author.tag } })
