0fd77e601417c976d674b075c4e6d66ce767270f
MARCROCK22 <marcoscolonrosario@gmail.com>
2021-02-24 17:57:41 -0400

    *

--- cmds/utiles/discordstatus.js
+++ cmds/utiles/discordstatus.js
@@ -9,7 +9,13 @@ module.exports = class Comando extends Command {
     }
     async run({ message }) {
         const url = `https://discordstatus.com`;
-        const data = await (await require('node-fetch')(url)).text()
+        const puppeteer = require('puppeteer')
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url, { waitUntil: 'domcontentloaded' });
+        await page.waitFor(1500);
+        const data = await page.content();
+        await browser.close();
         const Canvas = require('canvas'),
             canvas = Canvas.createCanvas(445, 60 * 5),
             ctx = canvas.getContext('2d')
@@ -29,6 +35,13 @@ module.exports = class Comando extends Command {
                 ctx.fillRect(array[i], n * 60, 3, 34)
             }
         }
+
+        let svg = data.split('<path fill="none" d="')[1].split('" class')[0]
+        const svg2img = require('node-svg2img')
+        let res = await require('util').promisify(svg2img)(`<svg><path fill="none" d="${svg}" class="highcharts-graph" data-z-index="1" stroke="#738bd7" stroke-width="2" stroke-linejoin="round" stroke-linecap="round"></path></svg>`, { format: 'png', width: 900, height: 100 })
+        res = await require('canvas').loadImage(res)
+        ctx.drawImage(res, 0, 320, 900, 100)
+
         let att = new Discord.MessageAttachment(canvas.toBuffer(), 'img.png');
         let embed = new Discord.MessageEmbed()
             .setColor(message.client.color)
