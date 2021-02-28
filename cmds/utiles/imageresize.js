dfe0370431774f7d9e443f7f0c7255e5bc928143
MARCROCK22 <marcoscolonrosario@gmail.com>
2021-02-27 21:20:58 -0400

    resize a gif

--- cmds/utiles/imageresize.js
+++ cmds/utiles/imageresize.js
@@ -45,6 +45,8 @@ module.exports = class Comando extends Command {
 
         if (require('is-gif')(bufferTest, 0, 3)) {
 
+            message.channel.send('Es gif...')
+
             const resize = require('@gumlet/gif-resize'),
                 res = await resize({ width: numerito, height: segundonumerito })(bufferTest);
 
@@ -53,6 +55,8 @@ module.exports = class Comando extends Command {
         }
 
         else {
+
+            message.channel.send('**No** es gif...')
 
             const Canvas = require('canvas');
 
