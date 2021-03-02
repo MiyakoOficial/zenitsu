8b9a5cda10ee6cabd45628ef3bccc5f3dee5f3bd
MARCROCK22 <marcoscolonrosario@gmail.com>
2021-03-02 16:56:30 -0400

    .

--- handler/eventos.js
+++ handler/eventos.js
@@ -13,6 +13,7 @@ module.exports = (client) => {
         for (let file of events) {
             const evt = require(`../events/${dirs}/${file}`);
             let eName = file.split('.')[0];
+            console.log(eName, evt)
             client.on(eName, evt.bind(null, client));
         }
     }
