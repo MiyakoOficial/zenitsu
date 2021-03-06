9e90b5882033f71f4946941050615c4fa85f3f69
MARCROCK22 <marcoscolonrosario@gmail.com>
2021-03-06 11:11:15 -0400

    1000 => 2700

--- cmds/utiles/imageresize.js
+++ cmds/utiles/imageresize.js
@@ -50,7 +50,7 @@ module.exports = class Comando extends Command {
                 .setImage(attachment)
                 .setColor(client.color)
                 .setTimestamp()
-                .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 2048 }))
+                .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 2048 }), attachment)
                 .setFooter(`La imagen solo durara un dia, si quieres puedes descargartela...`)
 
             return message.channel.send({ content: `${message.member}`, embed });
